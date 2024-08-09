4aa1838e10baa097f85b4c35678033f17c69fd6d
Hanaa Sadoqi <hsadoqi@gmail.com>
2024-08-09 18:57:36 -0400

    refactor: improve Icon component code

--- src/components/shared/Icon/Icon.tsx
+++ src/components/shared/Icon/Icon.tsx
@@ -1,133 +1,153 @@
 import React from "react";
 import { IconContext } from "react-icons";
 import clsx from "clsx";
+import { IconLibrary } from "./icons";
+import { ButtonSize } from "../BaseButton/BaseButton";
 
-/**
- * Props for the Icon component.
- *
- * @interface IconProps
- * @property { "svg" | "react-icons" } [type="react-icons"] - The type of icon being rendered. Defaults to "react-icons".
- * @property { React.ReactElement } [icon] - The React element to render for "react-icons" type. Required if type is "react-icons".
- * @property { string | number } [size] - The size of the icon. Can be a string or number.
- * @property { React.CSSProperties } [style] - Custom styles to apply to the icon.
- * @property { string } [className] - Additional CSS class names to apply to the icon wrapper.
- * @property { React.ReactNode } [children] - Children to render if type is "svg". Required if type is "svg".
- * @property { string } [ariaLabel] - The aria-label for accessibility purposes.
- * @property { boolean } [ariaHidden=false] - Whether the icon is hidden from screen readers.
- * @property { "img" | "presentation" | "graphic-symbol" } [role="img"] - The ARIA role of the icon.
- */
-interface IconProps {
-  type?: "svg" | "react-icons";
+export type IconType = "svg" | "react-icons";
+
+export interface IconProps {
+  type?: IconType;
   icon?: React.ReactElement;
-  size?: string | number;
+  size?: ButtonSize;
   style?: React.CSSProperties;
+  id?: string;
   className?: string;
   children?: React.ReactNode;
   ariaLabel?: string;
   ariaHidden?: boolean;
   role?: "img" | "presentation" | "graphic-symbol";
+  loading?: boolean;
 }
 
-/**
- * Icon component that supports both react-icons and custom SVG icons.
- *
- * @param {IconProps} props - The props for the component.
- * @returns {JSX.Element} The rendered icon component.
- */
 const Icon: React.FC<IconProps> = ({
   type = "react-icons",
   icon,
-  size,
+  size = "md",
   style,
-  className = "",
+  id,
+  className,
   children,
   ariaLabel,
   ariaHidden = false,
   role = "img",
+  loading = false,
 }) => {
-  // Common props applied to the wrapper element of the icon.
-  const sharedProps = {
-    "aria-hidden": ariaHidden,
-    role,
-    "data-testid": "icon-wrapper",
-    className: clsx(className, "icon-wrapper w-full h-full inline-block"),
+  const validTypes: IconType[] = ["svg", "react-icons"];
+
+  const iconSizes: Record<ButtonSize, string> = {
+    xs: "12px",
+    sm: "16px",
+    md: "20px",
+    lg: "24px",
+    xl: "32px",
+    full: "auto",
   };
 
-  // Styles to apply to the icon element.
+  if (!validTypes.includes(type)) {
+    console.warn(`Invalid type "${type}" provided to Icon component.`);
+    return null;
+  }
+
+  const sharedProps = {
+    "aria-hidden": ariaHidden,
+    "aria-label": ariaLabel,
+    role,
+    "data-testid": "icon-wrapper",
+    className: clsx("icon-wrapper inline-block", className),
+    style: {
+      width: size ? iconSizes[size] : "auto",
+      height: size ? iconSizes[size] : "auto",
+    },
+  };
+
   const iconStyle = {
     color: "currentColor",
     fill: "currentColor",
     ...style,
   };
 
-  /**
-   * Renders the icon when type is "react-icons".
-   *
-   * @returns {JSX.Element} The rendered react-icon component.
-   * @throws Will throw an error if `icon` prop is not provided.
-   */
   const renderReactIcon = () => {
-    if (!icon) {
-      throw new Error(
+    if (!icon && !loading) {
+      console.error(
         "Icon component requires an icon prop when type is react-icons",
       );
+      return null;
     }
 
-    return (
-      <IconContext.Provider
-        value={{
-          style: iconStyle,
-          size: "auto",
-        }}
-      >
-        <span {...sharedProps} aria-label={ariaLabel} tabIndex={-1}>
-          {React.cloneElement(icon, {
-            "data-testid": "icon",
-          })}
-        </span>
-      </IconContext.Provider>
+    const IconComponent = loading ? (
+      <IconLibrary.loading
+        className="animate-spin transition-transform duration-500"
+        data-testid="spinner"
+      />
+    ) : (
+      icon
     );
-  };
 
-  /**
-   * Renders the icon when type is "svg".
-   *
-   * @returns {JSX.Element} The rendered SVG icon component.
-   * @throws Will throw an error if `children` prop is not provided.
-   */
-  const renderSVGIcon = () => {
-    if (!children) {
-      throw new Error("Icon component requires children when type is svg");
-    }
     return (
-      <span {...sharedProps}>
-        {React.Children.map(children, (child) => {
-          if (React.isValidElement(child) && child.type === "svg") {
-            return React.cloneElement(child as React.ReactElement, {
-              style: {
-                width: "100%",
-                height: "auto",
-                ...iconStyle,
-              },
-              tabIndex: -1,
-              "aria-labelledby": ariaLabel ? "svg-title" : undefined,
-              "data-testid": "svg-icon",
-              children: (
-                <>
-                  {ariaLabel && <title id="svg-title">{ariaLabel}</title>}
-                  {child.props.children}
-                </>
-              ),
-            });
-          }
-          return child;
-        })}
+      <span
+        id={id}
+        {...sharedProps}
+        aria-label={loading ? "Loading" : ariaLabel}
+      >
+        <IconContext.Provider
+          value={{
+            style: iconStyle,
+            size: size ? iconSizes[size] : "auto",
+          }}
+        >
+          {React.cloneElement(IconComponent as React.ReactElement, {
+            "data-testid": loading ? "spinner" : "icon",
+          })}
+        </IconContext.Provider>
       </span>
     );
   };
 
-  // Render the appropriate icon based on the `type` prop.
-  return type === "react-icons" ? renderReactIcon() : renderSVGIcon();
+  const renderSVGIcon = () => {
+    if (!children) {
+      console.error("Icon component requires children when type is svg");
+      return null;
+    }
+
+    return (
+      <span {...sharedProps}>
+        {React.Children.map(children, (child) =>
+          React.isValidElement(child) && child.type === "svg"
+            ? React.cloneElement(child as React.ReactElement, {
+                style: {
+                  ...iconStyle,
+                  width: size ? iconSizes[size] : "auto",
+                  height: size ? iconSizes[size] : "auto",
+                },
+                tabIndex: -1,
+                "aria-labelledby": ariaLabel ? "svg-title" : undefined,
+                "data-testid": loading ? "spinner" : "svg-icon",
+                className: clsx(
+                  "user-select-none pointer-events-none",
+                  child.props.className,
+                ),
+                children: (
+                  <>
+                    {ariaLabel && <title id="svg-title">{ariaLabel}</title>}
+                    {child.props.children}
+                  </>
+                ),
+              })
+            : child,
+        )}
+      </span>
+    );
+  };
+
+  if (type === "react-icons") {
+    return renderReactIcon();
+  } else if (type === "svg") {
+    return renderSVGIcon();
+  } else {
+    console.error("Invalid type provided");
+    return null;
+  }
 };
 
 export default Icon;
