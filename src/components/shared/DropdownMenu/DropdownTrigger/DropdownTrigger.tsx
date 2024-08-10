6d9e85d3f2c23b4e5fd38e3bb1fb6a1160fca00f
Hanaa Sadoqi <hsadoqi@gmail.com>
2024-08-10 15:13:17 -0400

    feat: add DropdownTrigger component to make DropdownMenu more reusable; accounted for different types of buttons

---
+++ src/components/shared/DropdownMenu/DropdownTrigger/DropdownTrigger.tsx
@@ -0,0 +1,94 @@
+import React from "react";
+import { IconButton, IconButtonProps } from "../../Buttons/IconButton";
+import { LinkButton, LinkButtonProps } from "../../Buttons/LinkButton";
+import { BaseButton, BaseButtonProps } from "../../Buttons";
+
+// Base props shared among button types
+interface BaseTriggerProps {
+  ariaExpanded: boolean;
+  ariaControls: string;
+  buttonComponentType: "icon" | "link" | "button";
+  children?: React.ReactNode;
+}
+
+// Props for IconButton type
+interface IconButtonTriggerProps extends BaseTriggerProps {
+  buttonComponentType: "icon";
+  // icon: React.ReactElement;
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
+  iconButtonProps: IconButtonProps;
+}
+
+// Props for LinkButton type
+interface LinkButtonTriggerProps extends BaseTriggerProps {
+  buttonComponentType: "link";
+  href: string;
+  onClick?: React.MouseEventHandler<HTMLAnchorElement>;
+  linkButtonProps: LinkButtonProps;
+}
+
+// Props for native button type
+interface ButtonTriggerProps extends BaseTriggerProps {
+  buttonComponentType: "button";
+  buttonProps: BaseButtonProps;
+}
+
+// Union type for all possible button props
+type DropdownTriggerProps =
+  | IconButtonTriggerProps
+  | LinkButtonTriggerProps
+  | ButtonTriggerProps;
+
+const DropdownTrigger: React.FC<DropdownTriggerProps> = ({
+  ariaExpanded,
+  ariaControls,
+  buttonComponentType,
+  children,
+  ...rest
+}) => {
+  switch (buttonComponentType) {
+    case "icon":
+      const { onClick, iconButtonProps } = rest as IconButtonTriggerProps;
+      return (
+        <IconButton
+          aria-controls={ariaControls}
+          aria-expanded={ariaExpanded}
+          onClick={onClick}
+          {...iconButtonProps}
+        >
+          {children}
+        </IconButton>
+      );
+
+    case "button":
+      const { buttonProps } = rest as ButtonTriggerProps;
+      return (
+        <BaseButton
+          aria-controls={ariaControls}
+          aria-expanded={ariaExpanded}
+          {...buttonProps}
+        >
+          {children}
+        </BaseButton>
+      );
+
+    case "link":
+      const { onClick: linkOnClick, linkButtonProps } =
+        rest as LinkButtonTriggerProps;
+      return (
+        <LinkButton
+          aria-controls={ariaControls}
+          aria-expanded={ariaExpanded}
+          onClick={linkOnClick}
+          {...linkButtonProps}
+        >
+          {children}
+        </LinkButton>
+      );
+
+    default:
+      throw new Error("Invalid buttonComponentType");
+  }
+};
+
+export default DropdownTrigger;
