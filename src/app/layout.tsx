2b2d9feef2b22edce9825feb8a70a1e91b2475e0
Hanaa Sadoqi <hsadoqi@gmail.com>
2024-08-10 10:35:56 -0400

    styles: add basic layout styles to html, body, and main elements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={clsx("relative", inter.className)}>{children}</body>
+      <body className={`relative ${inter.className}`}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+"use client";
+
+import Header from "@/components/Header/Header";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex size-full flex-col items-center">
+      <Header />
+    </main>
   );
 }
