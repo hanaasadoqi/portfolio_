64e69048022c56cfc3c78408c9771b9d061719ce
Hanaa Sadoqi <hsadoqi@gmail.com>
2024-08-10 00:38:33 -0400

    styles: update styles for body element

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,23 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../styles/globals.css";
+import clsx from "clsx";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "Hanaa Sadoqi's Portfolio",
-	description: "Full-stack Web Developer",
+  title: "Hanaa Sadoqi's Portfolio",
+  description: "Full-stack Web Developer",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<body className={inter.className}>{children}</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body className={clsx("relative", inter.className)}>{children}</body>
+    </html>
+  );
 }
