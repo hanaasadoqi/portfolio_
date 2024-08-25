720c7e0030d7c23a8db1d493e5baecf9bce5020e
Hanaa Sadoqi <hsadoqi@gmail.com>
2024-08-25 01:21:32 -0400

    docs: prettier updates and added config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,20 +1,23 @@
-import type { StorybookConfig } from "@storybook/nextjs";
+import type { StorybookConfig } from '@storybook/nextjs'
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
   ],
   framework: {
-    name: "@storybook/nextjs",
-    options: {},
+    name: '@storybook/nextjs',
+    options: {
+      fastRefresh: false,
+    },
   },
   docs: {
     autodocs: true,
   },
-  staticDirs: ["../public"],
-};
-export default config;
+  staticDirs: ['../public'],
+}
+
+export default config
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,16 +1,18 @@
-import type { Preview } from "@storybook/react";
-import "../src/styles/globals.scss";
+import { Preview } from '@storybook/react'
+import '../src/styles/globals.scss'
 
 const preview: Preview = {
   parameters: {
+    actions: {
+      argTypesRegex: '^on[A-Z].*',
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
-        date: /Date$/i,
+        date: /Date$/,
       },
     },
   },
-  tags: ["autodocs"],
-};
+}
 
-export default preview;
+export default preview
