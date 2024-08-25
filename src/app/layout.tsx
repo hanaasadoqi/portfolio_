41418bfeddd627936db9735f02873d282a43f7bd
Hanaa Sadoqi <hsadoqi@gmail.com>
2024-08-25 01:47:12 -0400

    feat: add functions to fetch data and update dynamic imports and layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,57 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "../styles/globals.scss";
+import type { Metadata } from 'next'
+import { Poppins, Raleway, Merriweather } from 'next/font/google'
+import dynamic from 'next/dynamic'
+import clsx from 'clsx'
+import '../styles/globals.scss'
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-poppins',
+  weight: ['400', '500', '600', '700', '800'],
+})
+
+const raleway = Raleway({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-raleway',
+  weight: ['400', '500', '700'],
+})
+
+const merriweather = Merriweather({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-merriweather',
+  weight: ['300', '400', '700'],
+})
 
 export const metadata: Metadata = {
   title: "Hanaa Sadoqi's Portfolio",
-  description: "Full-stack Web Developer",
-};
+  description: 'Full-stack Web Developer',
+}
+
+const Header = dynamic(() => import('@/components/Header/Header'), {
+  ssr: true,
+})
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body className={`relative ${inter.className}`}>{children}</body>
+      <body
+        className={clsx(
+          'relative',
+          poppins.variable,
+          raleway.variable,
+          merriweather.variable
+        )}
+      >
+        <Header />
+        {children}
+      </body>
     </html>
-  );
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,105 @@
-"use client";
-import Section from "@/components/Section/Section";
-import styles from "../components/Section/Section.module.scss";
-import dynamic from "next/dynamic";
+import fs from 'fs'
+import path from 'path'
+import dynamic from 'next/dynamic'
 
-import Header from "@/components/Header/Header";
-
-const Hero = dynamic(() => import("@/components/Hero/Hero"), {
-  ssr: true,
-});
-const About = dynamic(() => import("@/components/About/About"), {
+// Dynamically import client components
+const Hero = dynamic(() => import('@/components/Hero/Hero'), { ssr: true })
+const About = dynamic(() => import('@/components/About/About'), { ssr: false })
+const Skills = dynamic(() => import('@/components/Skills/Skills'), {
   ssr: false,
-});
+})
+const Section = dynamic(() => import('@/components/Section/Section'), {
+  ssr: true,
+})
+const Carousel = dynamic(() => import('@/components/Experience/Carousel'), {
+  ssr: false,
+})
+const Projects = dynamic(() => import('@/components/Projects/Projects'), {
+  ssr: true,
+})
+const Education = dynamic(() => import('@/components/Education/Education'), {
+  ssr: true,
+})
+const Writing = dynamic(() => import('@/components/Writing/Writing'), {
+  ssr: true,
+})
+import Contact from '@/components/Contact/Contact'
 
+// Helper function to read data directly from the file system
+function fetchData(fileName: string) {
+  const filePath = path.join(process.cwd(), 'src/data', fileName)
+  const fileData = fs.readFileSync(filePath, 'utf-8')
+  return JSON.parse(fileData)
+}
+
+// Main server component for the home page
 export default function Home() {
-  const handleModal = () => {
-    console.log("open");
-  };
+  // Read data directly from JSON files during the build step
+  const aboutData = fetchData('aboutData.json')
+  const experienceData = fetchData('experience.json')
+  const skillsData = fetchData('skillsData.json')
+  const projectsData = fetchData('projects.json')
+  const writingData = fetchData('writing.json')
+  const educationData = fetchData('education.json')
+
+  // Server component function
   return (
-    <main className="flex min-h-screen flex-col items-center">
-      <Header />
-      <div className="">
-        <Section
-          id="hero"
-          label="Welcome to My Portfolio"
-          className="bg-hero-gradient"
-        >
-          <Hero />
+    <main className="flex min-h-screen w-full flex-col items-center overflow-hidden">
+      <div className="h-full w-full">
+        <Section id="hero" className="bg-hero-gradient">
+          <Hero
+            title="Welcome to My Portfolio"
+            subtitle="Full-Stack Developer | UI/UX Enthusiast | Lifelong Learner"
+            greeting="Hi, I'm Hanaa Sadoqi, a Full-Stack Developer passionate about creating high-quality, user-friendly web applications. Let's connect and build something amazing together."
+          />
         </Section>
         <Section
           id="about"
           label="A Little About Me"
-          subtitle="Snapshots of the things that bring me joy and define who I am."
-          description="These images represent the places, experiences, and people that shape my journey. I hope this small glimpse into my world brings a smile to your face."
+          subtitle="Snapshots of some things that interest me, inspire me or bring me joy."
           className="bg-about-gradient"
         >
-          <About />
+          <About aboutData={aboutData} />
         </Section>
         <Section
           id="skills"
           label="Skills"
-          className="bg-skills-gradient"
-        ></Section>
-        <Section id="experience" label="Work Experience"></Section>
-        <Section id="projects" label="Projects"></Section>
-        <Section id="writing" label="Writing"></Section>
-        <Section id="education" label="Education"></Section>
-        <Section id="contact" label="Contact Me"></Section>
+          className="bg-skills-gradient py-96"
+        >
+          <Skills skillsData={skillsData} />
+        </Section>
+        <Section
+          id="experience"
+          label="Work Experience"
+          className="bg-experience-gradient py-96"
+        >
+          <Carousel experienceData={experienceData} />
+        </Section>
+        <Section
+          id="projects"
+          label="Projects"
+          className="bg-projects-gradient"
+        >
+          <Projects projectsData={projectsData} />
+        </Section>
+        <Section id="writing" label="Writing" className="bg-writing-gradeint">
+          <Writing writingData={writingData} />
+        </Section>
+        <Section
+          id="education"
+          label="Education"
+          className="bg-education-gradient"
+        >
+          <Education educationData={educationData} />
+        </Section>
+        <Section
+          id="contact"
+          label="Contact Me"
+          className="bg-contact-gradient"
+        >
+          <Contact />
+        </Section>
       </div>
     </main>
-  );
+  )
 }
