5e7a3a5881fa1f295f139f54f707c6a5b5918ef9
Hanaa Sadoqi <hsadoqi@gmail.com>
2024-08-25 01:44:06 -0400

    feat: add scroll functionality to `Header`

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,27 +1,22 @@
-import React from "react";
-import {
-  ButtonGroup,
-  IconButton,
-  IconButtonProps,
-  LinkButton,
-  NavMenu,
-} from "../shared";
+"use client";
+
+import React, { useEffect, useState, useRef } from "react";
 import { FaCog, FaHome, FaLink } from "react-icons/fa";
 import { GiHamburgerMenu } from "react-icons/gi";
+import {
+  LinkButton,
+  NavMenu,
+  IconButton,
+  IconButtonProps,
+  ButtonGroup,
+} from "../shared";
+import clsx from "clsx";
 
 const navLinks = [
+  { href: "#about", label: "About", ariaLabel: "About Me Section" },
+  { href: "#skills", label: "Skills", ariaLabel: "Technical Skills Section" },
   {
-    href: "#about",
-    label: "About",
-    ariaLabel: "About Me Section",
-  },
-  {
-    href: "#skills",
-    label: "Skills",
-    ariaLabel: "Technical Skills Section",
-  },
-  {
-    href: "#work",
+    href: "#experience",
     label: "Experience",
     ariaLabel: "Work Experience Section",
   },
@@ -30,24 +25,16 @@ const navLinks = [
     label: "Projects",
     ariaLabel: "Personal Projects Section",
   },
-  {
-    href: "#writing",
-    label: "Writing",
-    ariaLabel: "Articles/Blogs Section",
-  },
-  {
-    href: "#education",
-    label: "Education",
-    ariaLabel: "Education Section",
-  },
-  {
-    href: "#contact",
-    label: "Contact",
-    ariaLabel: "Contact Me Section",
-  },
+  { href: "#writing", label: "Writing", ariaLabel: "Articles/Blogs Section" },
+  { href: "#education", label: "Education", ariaLabel: "Education Section" },
+  { href: "#contact", label: "Contact", ariaLabel: "Contact Me Section" },
 ];
 
 const Header: React.FC = () => {
+  const [currentSection, setCurrentSection] = useState<string | null>(null);
+  const [isScrollingDown, setIsScrollingDown] = useState(false);
+  const lastScrollTop = useRef(0);
+
   const iconButtonProps: Partial<IconButtonProps> = {
     variant: "text",
     size: "xs",
@@ -56,10 +43,60 @@ const Header: React.FC = () => {
     iconOnly: true,
   };
 
+  // Use Intersection Observer to track the current section
+  useEffect(() => {
+    const observerOptions = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.5,
+    };
+
+    const observerCallback = (entries: IntersectionObserverEntry[]) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          console.log(entry.target.id);
+          setCurrentSection(entry.target.id);
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(
+      observerCallback,
+      observerOptions,
+    );
+
+    const sections = document.querySelectorAll("section");
+    sections.forEach((section) => observer.observe(section));
+
+    return () => {
+      sections.forEach((section) => observer.unobserve(section));
+    };
+  }, []);
+
+  // Track scrolling direction
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY || document.documentElement.scrollTop;
+      setIsScrollingDown(scrollTop > lastScrollTop.current);
+      lastScrollTop.current = scrollTop <= 0 ? 0 : scrollTop;
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <header
       id="nav"
-      className="fixed left-0 right-0 flex items-center justify-between space-x-4 border-b bg-white/25 p-3 bg-blend-lighten drop-shadow-md backdrop-blur-3xl md:space-x-8 lg:space-x-12"
+      className={clsx(
+        "fixed left-0 right-0 z-50 flex items-center justify-between space-x-4 border-b bg-white/25 p-3 bg-blend-lighten drop-shadow-md backdrop-blur-3xl transition-transform duration-300",
+        {
+          "-translate-y-full opacity-0": isScrollingDown,
+          "translate-y-0 opacity-100": !isScrollingDown,
+        },
+      )}
     >
       <div id="logo-container">
         <LinkButton
@@ -71,7 +108,13 @@ const Header: React.FC = () => {
         />
       </div>
       <NavMenu
-        links={navLinks}
+        links={navLinks.map((link) => ({
+          ...link,
+          className:
+            currentSection === link.href.substring(1)
+              ? "bg-gray-300 text-gray-700 text-black font-bold"
+              : "",
+        }))}
         className="ease relative hidden w-full flex-1 flex-nowrap items-center justify-evenly p-1 backdrop-blur-sm transition-transform duration-300 lg:flex"
       />
       <div id="nav-icon-menu">
