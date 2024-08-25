6cfb01081969da80041281b78a587fbd7c889e34
Hanaa Sadoqi <hsadoqi@gmail.com>
2024-08-25 01:15:55 -0400

    build: add config for images used in Next.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,24 @@
+import withBundleAnalyzer from '@next/bundle-analyzer'
+
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "localhost:3000",
-      },
+    domains: [
+      'images.pexels.com',
+      'media.licdn.com',
+      'www.searchenginejournal.com',
+      'miro.medium.com',
+      'images.squarespace-cdn.com',
+      'localhost.com',
+      'cdn.pixabay.com',
     ],
   },
-};
+}
 
-export default nextConfig;
+export default bundleAnalyzer(nextConfig)
