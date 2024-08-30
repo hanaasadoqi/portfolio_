dfa5196fcb54ec195d78f23c556bbc75067c5666
Hanaa Sadoqi <hsadoqi@gmail.com>
2024-08-30 19:55:41 -0400

    feat: build out `Writing` component

--- src/components/Writing/Writing.tsx
+++ src/components/Writing/Writing.tsx
@@ -1,17 +1,86 @@
-import React from 'react'
-import WritingCard, { WritingItemProps } from './WritingCard'
+'use client'
 
-interface WritingProps {
-  writingData: WritingItemProps[]
-}
+import React, { useState, useEffect } from 'react'
+import { useMediaQuery } from 'react-responsive'
+import WritingCard from './WritingCard'
+import clsx from 'clsx'
+import { useData } from '@/context/DataContext'
 
-const Writing: React.FC<WritingProps> = ({ writingData: articles }) => {
+const Writing: React.FC = () => {
+  const { articles } = useData()
+  const [currentPage, setCurrentPage] = useState(1)
+  const [itemsPerPage, setItemsPerPage] = useState(4)
+
+  // Media queries to determine screen size
+  const isSmallScreen = useMediaQuery({ query: '(max-width: 768px)' }) // sm
+  const isMediumScreen = useMediaQuery({
+    query: '(min-width: 769px) and (max-width: 1024px)',
+  }) // lg
+  const isLargeScreen = useMediaQuery({ query: '(min-width: 1025px)' })
+
+  const isXLargeScreen = useMediaQuery({ query: '(min-width: 1600px)' })
+
+  // Adjust items per page based on screen size
+  useEffect(() => {
+    if (isSmallScreen) {
+      setItemsPerPage(1)
+    } else if (isMediumScreen) {
+      setItemsPerPage(2)
+    } else if (isLargeScreen) {
+      setItemsPerPage(3)
+    } else {
+      setItemsPerPage(4)
+    }
+  }, [isSmallScreen, isMediumScreen, isLargeScreen, isXLargeScreen])
+
+  // Calculate total pages
+  const totalPages = Math.ceil(articles.length / itemsPerPage)
+
+  // Calculate articles to display on current page
+  const startIndex = (currentPage - 1) * itemsPerPage
+  const currentArticles = articles.slice(startIndex, startIndex + itemsPerPage)
+
+  // Pagination handler
+  const handlePageChange = (pageNumber: number) => {
+    setCurrentPage(pageNumber)
+  }
+
   return (
-    <section className="from-primary-100 to-secondary-100 bg-gradient-to-b px-4 py-16">
-      {/* <h2 className="mb-12 text-center text-4xl font-bold">Writing</h2> */}
-      <div className="mx-auto grid grid-cols-2 gap-8 md:grid-cols-3 lg:grid-cols-4">
-        {articles.map(article => (
-          <WritingCard key={article.id} {...article} />
+    <section
+      id="writing"
+      data-id="writing"
+      className="mx-auto my-48 max-w-7xl px-4"
+    >
+      <h3 className="]text-primary-800 mb-4 text-center text-2xl dark:text-primary-200 md:text-left md:text-3xl lg:text-4xl">
+        Articles
+      </h3>
+      {/* Responsive Grid Layout for Cards */}
+      <div
+        className={clsx('mx-auto grid gap-8', {
+          'grid-cols-1': isSmallScreen,
+          'grid-cols-2': isMediumScreen,
+          'grid-cols-3': isLargeScreen,
+        })}
+      >
+        {currentArticles.map(item => (
+          <WritingCard key={item.id} {...item} />
+        ))}
+      </div>
+
+      {/* Responsive Pagination Controls */}
+      <div className="mt-8 flex flex-wrap items-center justify-center space-x-2">
+        {Array.from({ length: totalPages }, (_, index) => (
+          <button
+            key={index}
+            className={`mx-1 rounded-full px-3 py-1 text-sm ${
+              currentPage === index + 1
+                ? 'bg-primary-500 text-white'
+                : 'bg-gray-200 text-gray-700 dark:bg-gray-700 dark:text-gray-300'
+            }`}
+            onClick={() => handlePageChange(index + 1)}
+          >
+            {index + 1}
+          </button>
         ))}
       </div>
     </section>
--- src/components/Writing/WritingCard.tsx
+++ src/components/Writing/WritingCard.tsx
@@ -3,19 +3,9 @@
 import React from 'react'
 import Tag from '../shared/Tag'
 import Image from 'next/image'
+import { ArticleWithDetails } from '@/types/data'
 
-export interface WritingItemProps {
-  id: number
-  title: string
-  description: string
-  image: string
-  type: string
-  url: string
-  publishedDate: string
-  tags: string[]
-}
-
-const WritingCard: React.FC<WritingItemProps> = ({
+const WritingCard: React.FC<ArticleWithDetails & { parent?: string }> = ({
   id,
   title,
   description,
@@ -24,22 +14,24 @@ const WritingCard: React.FC<WritingItemProps> = ({
   url,
   publishedDate,
   tags,
+  parent,
 }) => {
   return (
-    <div className="flex cursor-pointer flex-col justify-evenly rounded-lg bg-white p-6 shadow-md transition-shadow hover:shadow-lg">
+    <div className="flex cursor-pointer flex-col justify-evenly rounded-lg bg-primary-100 p-6 text-primary-900 shadow-md transition-shadow hover:shadow-lg dark:bg-primary-800 dark:text-primary-100">
       <h3 className="mb-2 text-2xl font-semibold">{title}</h3>
       <p className="mb-4 text-sm text-gray-500">
         Published on {new Date(publishedDate).toLocaleDateString()}
       </p>
-      <Image
-        src={image}
-        alt={`${title} thumbnail`}
-        width={192}
-        height={192}
-        className="mb-4 h-48 w-full rounded-lg object-cover"
-        layout="responsive"
-      />
-      <p className="mb-4 text-gray-700">{description}</p>
+      {!parent && (
+        <Image
+          src={image}
+          alt={`${title} thumbnail`}
+          width={192}
+          height={192}
+          className="mb-4 h-48 w-full rounded-lg object-cover"
+        />
+      )}
+      <p className="mb-4 text-gray-700 dark:text-gray-300">{description}</p>
       <div className="flex flex-wrap gap-2">
         {tags.map((tag, index) => (
           <Tag key={index} tag={tag} />
