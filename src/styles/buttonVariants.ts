75cd32039ef596765d766fd91c84855f92a84655
Hanaa Sadoqi <hsadoqi@gmail.com>
2024-08-30 19:37:12 -0400

    styles: add styles for global styles and for button variants.
    TODO: Finalize design system for application

---
+++ src/styles/buttonVariants.ts
@@ -0,0 +1,11 @@
+
+export const buttonVariants = {
+  primary:
+    'bg-indigo-600 text-white hover:bg-indigo-500 active:bg-indigo-700 dark:bg-indigo-500 dark:hover:bg-indigo-400 dark:active:bg-indigo-600',
+  secondary:
+    'bg-gray-900 text-white hover:bg-gray-800 active:bg-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 dark:active:bg-gray-600',
+  outline:
+    'bg-transparent text-primary-500 border border-primary-500 hover:bg-primary-500 hover:text-white dark:text-primary-300 dark:border-primary-300 dark:hover:bg-primary-400 dark:hover:text-white',
+  danger:
+    'bg-red-600 text-white hover:bg-red-500 active:bg-red-700 dark:bg-red-500 dark:hover:bg-red-400 dark:active:bg-red-600',
+}
