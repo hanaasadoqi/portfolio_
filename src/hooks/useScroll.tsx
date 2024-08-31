ae664d8563c2bcb937c83514223dec254eff5c96
Hanaa Sadoqi <hsadoqi@gmail.com>
2024-08-30 20:12:00 -0400

    feat: add hooks for streamlined functonality shared across application

---
+++ src/hooks/useScroll.tsx
@@ -0,0 +1,49 @@
+import { useRef, useState, useEffect, useCallback } from 'react'
+
+const useScroll = () => {
+  const scrollRef = useRef<HTMLDivElement>(null)
+  const [scrollPosition, setScrollPosition] = useState({
+    left: false,
+    right: true,
+  })
+
+  const handleScroll = useCallback((direction: 'left' | 'right') => {
+    if (scrollRef.current) {
+      const { scrollLeft } = scrollRef.current
+      const scrollAmount = 500
+      const scrollTo =
+        direction === 'left'
+          ? scrollLeft - scrollAmount
+          : scrollLeft + scrollAmount
+
+      scrollRef.current.scrollTo({ left: scrollTo, behavior: 'smooth' })
+    }
+  }, [])
+
+  const checkScrollPosition = useCallback(() => {
+    if (scrollRef.current) {
+      const { scrollLeft, scrollWidth, clientWidth } = scrollRef.current
+      setScrollPosition({
+        left: scrollLeft > 0,
+        right: scrollLeft + clientWidth < scrollWidth,
+      })
+    }
+  }, [])
+
+  useEffect(() => {
+    const scrollContainer = scrollRef.current
+    if (!scrollContainer) return // Safeguard against null value, just in case
+
+    const throttledCheckScroll = () => checkScrollPosition()
+    scrollContainer.addEventListener('scroll', throttledCheckScroll)
+    checkScrollPosition() // Initial check
+
+    return () => {
+      scrollContainer.removeEventListener('scroll', throttledCheckScroll)
+    }
+  }, [checkScrollPosition])
+
+  return { scrollRef, scrollPosition, handleScroll }
+}
+
+export default useScroll
