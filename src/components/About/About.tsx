4d6cb4b079c320629b28a7de7abcc5bc6af6c9dd
Hanaa Sadoqi <hsadoqi@gmail.com>
2024-08-30 20:01:25 -0400

    feat: build out `About` component with custon carousel functionality

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,153 +1,66 @@
 'use client'
 
-import React, { useRef, useState, useEffect } from 'react'
-import Image from 'next/image'
-import { Video } from '../shared/Media'
+import React, { Suspense, memo } from 'react'
+import { About as AboutItem } from '@/types/data'
+import useScroll from '@/hooks/useScroll'
+import ScrollButton from '@/components/UI/ScrollButton'
+import AboutItemCard from '@/components/About/AboutItemCard'
+import { useData } from '@/context/DataContext'
 
-interface AboutItem {
-  id: number
-  title: string
-  description: string
-  type: 'image' | 'video'
-  src: string
-  poster: string
-  alt: string
-  rows: number
-  columns: number
-}
+const About: React.FC = () => {
+  const { about } = useData()
+  const { scrollRef, scrollPosition, handleScroll } = useScroll()
 
-interface AboutProps {
-  aboutData: AboutItem[]
-}
-
-const About: React.FC<AboutProps> = ({ aboutData: items }) => {
-  const scrollRef = useRef<HTMLDivElement>(null)
-  const [showLeftButton, setShowLeftButton] = useState(false)
-  const [showRightButton, setShowRightButton] = useState(true)
-
-  // Function to handle scrolling
-  const scroll = (direction: 'left' | 'right') => {
-    if (scrollRef.current) {
-      const { scrollLeft, scrollWidth, clientWidth } = scrollRef.current
-      const scrollAmount = 300
-      const scrollTo =
-        direction === 'left'
-          ? scrollLeft - scrollAmount
-          : scrollLeft + scrollAmount
-
-      scrollRef.current.scrollTo({ left: scrollTo, behavior: 'smooth' })
-    }
-  }
-
-  // Function to check scroll position
-  const checkScrollPosition = () => {
-    if (scrollRef.current) {
-      const { scrollLeft, scrollWidth, clientWidth } = scrollRef.current
-      setShowLeftButton(scrollLeft > 0)
-      setShowRightButton(scrollLeft + clientWidth < scrollWidth)
-    }
-  }
-
-  // Set up an effect to check scroll position on scroll and on load
-  useEffect(() => {
-    const scrollContainer = scrollRef.current
-    if (scrollContainer) {
-      scrollContainer.addEventListener('scroll', checkScrollPosition)
-      checkScrollPosition() // Initial check on mount
-    }
-    return () => {
-      if (scrollContainer) {
-        scrollContainer.removeEventListener('scroll', checkScrollPosition)
-      }
-    }
-  }, [])
-
+  const scrollButtonStyles =
+    'dark:text-white bg-white text-secondary-900 dark:bg-secondary-200 dark:text-secondary-900 dark:hover:bg-secondary-700 dark:hover:text-secondary-100 hover:bg-secondary-700 hover:text-secondary-100 active:bg-secondary-900 focus-visible:bg-secondary-900 text-primary-100 dark:bg-secondary-900'
   return (
-    <div className="relative mx-auto h-full w-full bg-gradient-to-r from-gray-100 to-gray-200 p-12">
-      {/* Left Scroll Button */}
-      {showLeftButton && (
-        <button
-          onClick={() => scroll('left')}
-          className="absolute left-4 top-1/2 z-10 -translate-y-1/2 transform rounded-full bg-white p-3 shadow-lg transition duration-300 ease-in-out hover:bg-gray-300"
-          aria-label="Scroll Left"
-        >
-          &lt;
-        </button>
-      )}
+    <section
+      id="about"
+      data-id="about"
+      className="my-48 flex min-h-screen w-screen items-center"
+    >
+      <div className="relative mx-auto w-full text-left">
+        <div className="mb-8 space-y-2 px-12 text-center md:mb-12 md:text-left">
+          <h3>A Little About Me</h3>
+          <h5>
+            Snapshots of things that interest me, inspire me, or bring me joy.
+          </h5>
+        </div>
 
-      {/* Right Scroll Button */}
-      {showRightButton && (
-        <button
-          onClick={() => scroll('right')}
-          className="absolute right-4 top-1/2 z-10 -translate-y-1/2 transform rounded-full bg-white p-3 shadow-lg transition duration-300 ease-in-out hover:bg-gray-300"
-          aria-label="Scroll Right"
-        >
-          &gt;
-        </button>
-      )}
+        {/* Render scroll buttons based on scroll position */}
+        <ScrollButton
+          direction="left"
+          onClick={() => handleScroll('left')}
+          visible={scrollPosition.left}
+          className={scrollButtonStyles}
+        />
+        <ScrollButton
+          direction="right"
+          onClick={() => handleScroll('right')}
+          visible={scrollPosition.right}
+          className={scrollButtonStyles}
+        />
 
-      {/* Container for horizontal scroll */}
-      <div
-        ref={scrollRef}
-        className="scrollbar-hide overflow-x-auto overflow-y-scroll whitespace-nowrap p-8 md:overflow-y-hidden"
-        style={{ scrollBehavior: 'smooth' }} // Ensures smooth scrolling behavior
-      >
-        {/* Grid Container for Items */}
+        {/* Scrollable container for about items */}
         <div
-          className="grid auto-cols-max grid-flow-row grid-cols-2 items-center justify-items-center gap-6 md:grid-flow-col md:grid-rows-5"
-          style={{
-            gridTemplateRows: 'repeat(2, minmax(200px, 1fr))', // Adjust for multiple rows
-            minWidth: 'max-content', // Ensures the grid container is wide enough for horizontal scroll
-          }}
+          ref={scrollRef}
+          className="scrollbar-hide snap-x snap-mandatory overflow-x-auto overflow-y-hidden whitespace-nowrap p-8 md:snap-none"
         >
-          {items.map(item => (
-            <div
-              key={item.id}
-              className="relative flex-shrink-0 rounded-xl p-2 transition-transform duration-300 hover:z-10 hover:scale-105"
-              style={{
-                width: `${item.columns * 100}px`,
-                height: `${item.rows * 100}px`,
-              }}
-            >
-              {/* Card Container */}
-              <div className="group relative h-full w-full [perspective:1000px]">
-                <div className="relative h-full w-full rounded-xl shadow-lg ring-offset-secondary transition-all duration-700 [transform-style:preserve-3d] group-hover:shadow-2xl group-hover:ring-2 group-hover:ring-offset-4 group-hover:[transform:rotateY(180deg)]">
-                  {/* Front Side */}
-                  <div className="absolute inset-0 h-full w-full overflow-hidden rounded-xl">
-                    {item.type === 'image' && (
-                      <Image
-                        src={item.src}
-                        alt={item.alt || ''}
-                        fill
-                        className="rounded-xl object-cover transition duration-300 group-hover:brightness-90"
-                        sizes="(max-width: 500px) 100vw, (max-width: 750px) 50vw, 33vw"
-                      />
-                    )}
-                    {item.type === 'video' && (
-                      <Video
-                        src={item.src}
-                        poster={item.poster}
-                        className="h-full w-full rounded-xl object-cover transition duration-300 group-hover:brightness-90"
-                      />
-                    )}
-                  </div>
-                  {/* Back Side */}
-                  <div className="absolute inset-0 h-full w-full rounded-xl bg-gradient-to-br from-purple-700 to-indigo-500 p-4 text-center text-white [backface-visibility:hidden] [transform:rotateY(180deg)]">
-                    <div className="flex h-full w-full items-center justify-center overflow-hidden whitespace-nowrap text-wrap text-white">
-                      <div className="mx-4 px-4 text-center">
-                        <h5 className="text-white">{item.title}</h5>
-                        <p className="text-white">{item.description}</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+          <Suspense fallback={<div>Loading items...</div>}>
+            <div className="grid snap-x snap-mandatory auto-cols-max grid-flow-col items-center gap-4">
+              {about.map(item => (
+                <AboutItemCard
+                  key={item.id}
+                  item={item}
+                  className="snap-center"
+                />
+              ))}
             </div>
-          ))}
+          </Suspense>
         </div>
       </div>
-    </div>
+    </section>
   )
 }
 
-export default About
+export default memo(About)
