31c8f503c1bac97f727742404ae5f620fd2fc1c4
Hanaa Sadoqi <hsadoqi@gmail.com>
2024-09-21 16:28:41 -0400

    fix: update `Dark Mode` to initialize correctly with appropriate functionality

--- src/context/DarkModeContext.tsx
+++ src/context/DarkModeContext.tsx
@@ -1,7 +1,5 @@
 'use client'
 
-'use client'
-
 import React, { createContext, useContext, useEffect, useState, useMemo, useCallback } from 'react'
 
 interface DarkModeContextProps {
@@ -12,21 +10,24 @@ interface DarkModeContextProps {
 const DarkModeContext = createContext<DarkModeContextProps | undefined>(undefined)
 
 export const DarkModeProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const [isDarkMode, setIsDarkMode] = useState<boolean>(false)
-
-  useEffect(() => {
-    // Check localStorage on client side
-    const savedTheme = typeof window !== 'undefined' ? localStorage.getItem('theme') : null
-    if (savedTheme) {
-      setIsDarkMode(savedTheme === 'dark')
-    } else {
-      // Fallback or default behavior if no saved theme is present
-      setIsDarkMode(false) // or true, depending on your default preference
+  // Initialize dark mode based on localStorage or system preference
+  const getInitialTheme = () => {
+    if (typeof window !== 'undefined') {
+      const savedTheme = localStorage.getItem('theme')
+      if (savedTheme) {
+        return savedTheme === 'dark'
+      } else {
+        // Use system preference for the first load if no theme is saved
+        return window.matchMedia('(prefers-color-scheme: dark)').matches
+      }
     }
-  }, [])
+    return false // Default to light mode if SSR or no theme preference
+  }
 
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(getInitialTheme)
+
+  // Effect to update localStorage and HTML class when theme changes
   useEffect(() => {
-    // Update localStorage and class on document
     if (typeof window !== 'undefined') {
       localStorage.setItem('theme', isDarkMode ? 'dark' : 'light')
       document.documentElement.classList.toggle('dark', isDarkMode)
