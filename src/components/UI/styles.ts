a41365111bc531d69bb0b59ca3176dd4f641b87f
Hanaa Sadoqi <hsadoqi@gmail.com>
2024-09-21 15:34:10 -0400

    chore: clean up unnecessary files

--- src/components/UI/styles.ts
+++
@@ -1,9 +0,0 @@
-export const variantClasses = {
-  primary:
-    'bg-primary-500 text-white hover:bg-primary-600 active:bg-primary-700',
-  secondary: 'bg-gray-900 text-white hover:bg-gray-800 active:bg-gray-700',
-  outline:
-    'bg-white text-primary-500 border border-primary-500 hover:bg-primary-500 hover:text-white',
-  danger: 'bg-red-500 text-white hover:bg-red-600 active:bg-red-700',
-  icon: 'text-primary-500 hover:text-primary-600 active:text-primary-700 dark:hover:text-primary-200 dark:active:text-primary-100',
-}
--- src/components/shared/BaseComponent/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as BaseComponent } from './BaseComponent'
-export * from './BaseComponent.types'
-export * from './BaseComponent'
-export * from './baseStyles'
