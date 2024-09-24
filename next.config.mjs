ea9632899239ffbfef04e5c440e5386a2263d8b1
Hanaa Sadoqi <hsadoqi@gmail.com>
2024-09-23 20:17:54 -0400

    build: update scripts to trace deprecation warnings

--- next.config.mjs
+++ next.config.mjs
@@ -19,7 +19,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 const nextConfig = {
-  reactProductionProfiling: true,
+  // reactProductionProfiling: true,
   reactStrictMode: true,
   swcMinify: true,
   images: {
