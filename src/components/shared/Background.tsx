5c56b8812c7c56f956f2305bd1b7561b45eb986e
Hanaa Sadoqi <hsadoqi@gmail.com>
2024-09-25 20:22:56 -0400

    styles: apply `skills` gradient to background of `writing` components

--- src/components/shared/Background.tsx
+++ src/components/shared/Background.tsx
@@ -11,9 +11,8 @@ const Background: React.FC<{ children: React.ReactNode, id?: string }> = ({ chil
   return (
     <div data-id={id} id={id} className={clsx("relative min-h-screen bg-cover", {
       "dark:bg-app-gradient-dark bg-app-gradient-light": id === 'app-wide',
-      "dark:from-primary-900 dark:to-secondary-800 from-primary-300 to-secondary-300": id === "writing",
-      "bg-projects-gradient dark:bg-projects-gradient-dark": id === "projects",
-      "bg-skills-gradient dark:bg-skills-gradient-dark": id === "skills"
+      "bg-skills-gradient dark:bg-skills-gradient-dark": id === "writing",
+      "bg-projects-gradient dark:bg-projects-gradient-dark": id === "projects"
     })}>
       <div className="pointer-events-none absolute opacity-85 inset-0 overflow-hidden">
         {stars}
