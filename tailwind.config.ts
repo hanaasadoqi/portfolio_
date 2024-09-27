1cb5d0a10db157e9efb8fea2b683212defbc0b35
Hanaa Sadoqi <hsadoqi@gmail.com>
2024-09-27 17:37:38 -0400

    styles: update colors for light and dark theme using predefined tailwind colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss'
+import colors from 'tailwindcss/colors';
 
 const config: Config = {
   darkMode: 'class',
@@ -10,7 +11,6 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        primary: 'var(--primary)',
         'primary-50': 'var(--primary-50)',
         'primary-100': 'var(--primary-100)',
         'primary-200': 'var(--primary-200)',
@@ -22,7 +22,6 @@ const config: Config = {
         'primary-800': 'var(--primary-800)',
         'primary-900': 'var(--primary-900)',
         'primary-950': 'var(--primary-950)',
-        secondary: 'var(--secondary)',
         'secondary-50': 'var(--secondary-50)',
         'secondary-100': 'var(--secondary-100)',
         'secondary-200': 'var(--secondary-200)',
@@ -34,38 +33,32 @@ const config: Config = {
         'secondary-800': 'var(--secondary-800)',
         'secondary-900': 'var(--secondary-900)',
         'secondary-950': 'var(--secondary-950)',
+        'accent-primary': colors.cyan,
+        'accent-secondary': colors.pink,
       },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
         'app-gradient-light':
-          'linear-gradient(to bottom, #cbd5e1, #bfdbfe, #a5b4fc, #f0abfc, #fbcfe8, #a5f3fc, #d8b4fe, #06b6de)',
+          'linear-gradient(to bottom, var(--hero-300), var(--about-300), var(--skills-300), var(--experience-300), var(--projects-300), var(--writing-300), var(--education-300), var(--neutral-300))',
         'app-gradient-dark':
-          'linear-gradient(to bottom, #0f172a, #1e3a8a, #4338ca, #e879f9, #ec4899, #6d28d9, #0c4a6e, #0891b2)',
-        'hero-light': 'linear-gradient(to bottom, #f1f5f9, #bfdbfe)',
-        'hero-dark': 'linear-gradient(to bottom, #0f172a, #1e3a8a)',
+          'linear-gradient(to bottom, var(--hero-950), var(--about-950), var(--skills-950), var(--experience-950), var(--projects-950), var(--writing-950), var(--education-950), var(--neutral-950))',
 
-        'hero-gradient': 'linear-gradient(to bottom, #f1f5f9, #bfdbfe)',
-        'about-gradient': 'linear-gradient(to bottom, #bfdbfe, #a5b4fc)',
-        'skills-gradient': 'linear-gradient(to bottom, #a5b4fc, #d8b4fe)',
-        'experience-gradient': 'linear-gradient(to bottom, #d8b4fe, #fbcfe8)',
-        'projects-gradient': 'linear-gradient(to bottom, #fbcfe8, #a5f3fc)',
-        'writing-gradient': 'linear-gradient(to bottom, #a5f3fc, #d8b4fe)',
-        'education-gradient': 'linear-gradient(to bottom, #d8b4fe, #67e8f9)',
-        'contact-gradient': 'linear-gradient(to bottom, #67e8f9, #06b6de)',
-
-        'hero-gradient-dark': 'linear-gradient(to bottom, #0f172a, #1e3a8a)',
-        'about-gradient-dark': 'linear-gradient(to bottom, #1e3a8a, #4338ca)',
-        'skills-gradient-dark': 'linear-gradient(to bottom, #4338ca, #e879f9)',
-        'experience-gradient-dark':
-          'linear-gradient(to bottom, #e879f9, #ec4899)',
-        'projects-gradient-dark':
-          'linear-gradient(to bottom, #ec4899, #6d28d9)',
-        'writing-gradient-dark': 'linear-gradient(to bottom, #6d28d9, #0c4a6e)',
-        'contact-gradient-dark': 'linear-gradient(to bottom, #0891b2, #22d3ee)',
-        'education-gradient-dark':
-          'linear-gradient(to bottom, #0c4a6e, #0891b2)',
+        'hero-light': 'linear-gradient(to bottom, var(--hero-300), var(--about-300))',
+        'hero-dark': 'linear-gradient(to bottom, var(--hero-950), var(--about-950))',
+        'about-light': 'linear-gradient(to bottom, var(--about-300), var(--skills-300))',
+        'about-dark': 'linear-gradient(to bottom, var(--about-950), var(--skills-950))',
+        'skills-light': 'linear-gradient(to bottom, var(--skills-300), var(--experience-300))',
+        'skills-dark': 'linear-gradient(to bottom, var(--skills-950), var(--experience-950))',
+        'experience-light': 'linear-gradient(to bottom, var(--experience-300), var(--projects-300))',
+        'experience-dark': 'linear-gradient(to bottom, var(--experience-950), var(--projects-950))',
+        'projects-light': 'linear-gradient(to bottom, var(--projects-300), var(--writing-300))',
+        'projects-dark': 'linear-gradient(to bottom, var(--projects-950), var(--writing-950))',
+        'writing-light': 'linear-gradient(to bottom, var(--writing-300), var(--education-300))',
+        'writing-dark': 'linear-gradient(to bottom, var(--writing-950), var(--education-950))',
+        'education-light': 'linear-gradient(to bottom, var(--education-300), var(--hero-300))',
+        'education-dark': 'linear-gradient(to bottom, var(--education-950), var(--hero-950))',
       },
       boxShadow: {
         'inner-lg':
