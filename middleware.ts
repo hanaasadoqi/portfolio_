9551009a0c14d3d88e6eb27318924012a3c32dc7
Hanaa Sadoqi <hsadoqi@gmail.com>
2024-11-22 16:15:41 -0500

    refactor: add styles and tweak middleware and packages

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,18 @@
-import { type NextRequest } from 'next/server'
+import { NextResponse, type NextRequest } from 'next/server'
 import { updateSession } from './src/app/lib/supabase/middleware'
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request)
+  const response = NextResponse.next();
+
+  response.headers.set(
+    'Content-Security-Policy',
+    `default-src 'self'; style-src 'self' 'unsafe-inline'`
+  );
+
+  return response;
 }
+  // return await updateSession(request)
+// }
 
 export const config = {
   matcher: [
